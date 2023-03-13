e7a37437bce5b9134b73fa326895552ee535e6f9
Oakar <linn.998.mlm@gmail.com>
2023-03-13 07:40:35 +0630

    This is first Commit in dom pt

--- app.js
+++ app.js
@@ -9,6 +9,55 @@ const output = document.querySelector("#output");
 const count = document.querySelector("#count");
 const color = document.querySelector("#color");
 const fontSize = document.querySelector("#fontSize");
+const textToSpeech = document.querySelector("#textToSpeech")
+const speechToText = document.querySelector("#speechToText")
+const synth = window.speechSynthesis;
+
+const listen = () => {
+  var recognition = new webkitSpeechRecognition();
+
+  //set language and recognizing
+  recognition.lang = "en-US";
+  recognition.start();
+
+  recognition.addEventListener("start",()=>{
+    speechToText.classList.add("active");
+    speechToText.innerHTML = `
+    <div class="spinner-border spinner-border-sm" role="status">
+  <span class="visually-hidden">Loading...</span>
+</div>
+    `
+  });
+  recognition.addEventListener("end", () =>{
+    speechToText.classList.remove("active");
+    speechToText.innerHTML = `<i class="bi bi-mic"></i>`;
+  });
+
+  //when a speech result is returned
+  recognition.onresult = function (event) {
+    //Get the transcript of the spoken words
+    var transcript = event.results[0][0].transcript;
+    //Display the transcript in the text box
+    text.value = transcript;
+  };
+  
+};
+speechToText.addEventListener("click", listen);
+
+
+const speak = (text) =>{
+  const utterThis = new SpeechSynthesisUtterance();
+  utterThis.rate = 0.9;
+  utterThis.text = text;
+  utterThis.voice = synth.getVoices()[0];
+  utterThis.addEventListener("start",()=>{
+    textToSpeech.classList.add("active");
+  });
+  utterThis.addEventListener("end",()=>{
+    textToSpeech.classList.remove("active");
+  });
+  synth.speak(utterThis);
+};
 
 fonts.forEach((font) => {
   fontFamily.append(new Option(font, font));
@@ -23,12 +72,16 @@ text.addEventListener("keyup", (event) => {
   count.innerText = text.value.length;
 });
 
-color.addEventListener("change", () => {
-  output.style.color = color.value;
+color.addEventListener("change", (event) => {
+  output.style.color = event.target.value;
 });
 fontSize.addEventListener("change", (event) => {
   output.style.fontSize = event.target.value + "px";
 });
 fontFamily.addEventListener("change",(event)=>{
     output.style.fontFamily = event.target.value;
+});
+
+textToSpeech.addEventListener("click",()=>{
+  speak(text.value);
 });
